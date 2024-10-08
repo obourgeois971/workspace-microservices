9340c2a55b0bcf3454b968461cb9c204f5f121a1
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-10-08 13:54:30 +0200

    inventory service init

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,32 @@
 package com.programmingtechie.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.programmingtechie.inventoryservice.model.Inventory;
+import com.programmingtechie.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_13");
+			inventory1.setQuantity(100);
+			
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("iphone_13_red");
+			inventory2.setQuantity(0);
+			
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+		};
+	}
 }
--- product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -7,12 +7,14 @@ import com.programmingtechie.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class ProductService {
 
     private final ProductRepository productRepository;
